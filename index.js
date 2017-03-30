208d2870eb3dc88d23d7e53e904f0a5b38abdd30
vadxq <862235971@qq.com>
2017-03-30 22:15:15 +0800

    finish v1.0

--- index.js
+++ index.js
@@ -3,22 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { DatePicker } from 'antd';
 
-function range(start, end) {
-  const result = [];
-  for (let i = start; i < end; i++) {
-    result.push(i);
-  }
-  return result;
-}
-
 function disabledDate(current) {
   // can not select days before today and today
-  // let firstDate = new Date.now() - 1
-  let lastDate = new Date('August 31,2017 23:59:59')
-
-  // console.log(current)
-  return current.valueOf() < Date.now() && current.valueOf() > lastDate.getTime();
-  
+  let lastDate = new Date('August 31,2017 23:59:59') // lastDate set
+  return current && current.valueOf() < Date.now() - 86400 || current && current.valueOf() > lastDate.getTime();
 }
 
 ReactDOM.render(
